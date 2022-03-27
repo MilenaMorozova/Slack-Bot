0510fcb8d831ec21214b6828279aa38cbf148c1d
Milena Morozova <milena.morozova@orioninc.com>
2022-03-27 18:04:57 +0300

    added functions for to validate and update the bot's manifest

---
+++ bot_manifest/update_manifest.py
@@ -0,0 +1,46 @@
+import json
+
+import requests
+
+from constants import HOST, ACCESS_TOKEN, APP_ID
+
+
+def replace_host_in_manifest():
+    with open("manifest.json", 'r') as file:
+        content = file.read()
+        content = content.replace("<host>", HOST)
+
+    return content
+
+
+def validate_bot_manifest(manifest_content):
+    response = requests.post("https://slack.com/api/apps.manifest.validate",
+                             data={"token": ACCESS_TOKEN,
+                                   "app_id": APP_ID,
+                                   "bot_manifest": manifest_content})
+
+    response_data = json.loads(response.content)
+    if not response_data["ok"]:
+        print(response_data["errors"])
+        return False
+
+    return True
+
+
+def update_bot_manifest():
+    manifest_content = replace_host_in_manifest()
+
+    if not validate_bot_manifest(manifest_content):
+        return False
+
+    response = requests.post("https://slack.com/api/apps.manifest.update",
+                             data={"token": ACCESS_TOKEN,
+                                   "app_id": APP_ID,
+                                   "bot_manifest": manifest_content})
+
+    response_data = json.loads(response.content)
+    return response_data["ok"]
+
+
+# manifest_content = replace_host_in_manifest()
+# print(update_bot_manifest())
